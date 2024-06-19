3d257c9d497797ba0a4e8ecd2d313d524032143d
SickLuca <repupilliluca.projects@gmail.com>
2024-06-20 01:22:08 +0200

    Web

---
+++ Unicam.Progetto.Libreria.Test/RepositoryExample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unicam.Progetto.Libreria.Abstractions;
+using Unicam.Progetto.Libreria.Entities;
+using Unicam.Progetto.Libreria.Models.Context;
+using Unicam.Progetto.Libreria.Models.Entities;
+using Unicam.Progetto.Libreria.Models.Repositories;
+
+namespace Unicam.Progetto.Libreria.Test
+{
+    public class RepositoryExample : IExample
+    {
+        public async Task RunExampleAsync()
+        {
+
+        }
+        public void RunExample()
+        {
+            var ctx = new MyDbContext();
+            var libroRepo = new LibroRepository(ctx);
+            var categoriaRepo = new CategoriaRepository(ctx);
+            var utenteRepo = new UtenteRepository(ctx);
+
+            var libro = libroRepo.Ottieni(1);
+            var categoria = categoriaRepo.Ottieni(1);
+            libro.Nome = "nuovo";
+            libroRepo.Modifica(libro);
+            libroRepo.Save();
+
+            var nuovaCategoria = new Categoria();
+            nuovaCategoria.NomeCategoria = "Fiction";
+            //nuovoDipendente.IdAzienda = 5;
+
+            var nuovoUtente = new Utente();
+            nuovoUtente.Nome = "Mario";
+            nuovoUtente.Cognome = "Rossi";
+            nuovoUtente.Email = "mario.rossi@gmail.com";
+            nuovoUtente.Password= "Camerino";
+           
+
+            utenteRepo.Aggiungi(nuovoUtente);
+            utenteRepo.Save();
+
+
+        }
+
+
+    }
+}
---
+++ Unicam.Progetto.Libreria.Web/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// INIZIALIZZO I SERVIZI
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//INIZIALIZZO I MIDDLEWARE
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
